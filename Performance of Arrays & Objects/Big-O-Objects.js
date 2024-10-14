51c7e95816736604e07417051165770dfbcb06b7
Am1d1v <dimadiman1adima@gmail.com>
2024-10-14 18:28:30 +0300

    The Big O of Objects

---
+++ Performance of Arrays & Objects/Big-O-Objects.js
@@ -0,0 +1,11 @@
+
+
+const person = {
+    name: 'Alice',
+    human: true,
+    favouriteNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+};
+
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(Object.entries(person));
