7c2a3250c18df8b3bf4c7538c32622ff59b7300a
Am1d1v <dimadiman1adima@gmail.com>
2024-10-14 19:02:14 +0300

    Big O of Array Methods

---
+++ Performance of Arrays & Objects/Big-O-Arrays.js
@@ -0,0 +1,11 @@
+
+
+const arrayOfName = ['Alice', 'Melissa', 'Dima'];
+
+//arrayOfName.shift();
+//arrayOfName.unshift('D');
+//console.log(arrayOfName.indexOf('Dima'))
+//console.log(arrayOfName.slice(0, 1), arrayOfName);
+//console.log(arrayOfName.splice(0, 2), arrayOfName);
+//console.log(arrayOfName.sort())
+console.log(arrayOfName.concat([1, 2, 3]));
--- Performance of Arrays & Objects/Big-O-Objects.js
+++ Performance of Arrays & Objects/Big-O-Objects.js
@@ -6,6 +6,7 @@ const person = {
     favouriteNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 };
 
-console.log(Object.keys(person));
-console.log(Object.values(person));
-console.log(Object.entries(person));
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+// console.log(Object.entries(person));
+console.log(person.hasOwnProperty('human'));
